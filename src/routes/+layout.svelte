d7c07e53e5a3074ed7cf5dc1d1374d7012c03e35
Ruben Hensen <ruben.hensen@protonmail.com>
2026-04-18 11:25:48 +0200

    feat: add purple PostGuard shield favicon as 144x144 PNG

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,8 @@
 <script lang="ts">
 	import '$lib/global.scss';
-	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 
 <div id="app">
 	{@render children()}
